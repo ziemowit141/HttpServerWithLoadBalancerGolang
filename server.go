17626b8aa7aa6e7a555b903e21cc70f84005fb5b
ziemowit141 <ziemowit141@gmail.com>
2021-07-13 11:45:26 +0200

    Parse request data into 'Person' structure

--- server.go
+++ server.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type Person struct {
+	ID int `json:"ID"`
+	Name string `json:"Name"`
+	EmailAddress string `json:"EmailAddress"`
+	Phone string `json:"Phone"`
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
@@ -15,7 +23,9 @@ func addUserHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
 		reqBody, _ := ioutil.ReadAll(r.Body)
-		fmt.Println(string(reqBody))
+		var person Person
+		json.Unmarshal(reqBody, &person)
+		fmt.Println(person.ID, person.Name, person.EmailAddress, person.Phone)
 	default:
 		fmt.Fprintf(w, "Sorry, only POST method is supported.")
 	}

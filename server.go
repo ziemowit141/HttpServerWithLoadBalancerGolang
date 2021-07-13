0e68ede8589f7fdf4429116bf3bf962a274c45a2
ziemowit141 <ziemowit141@gmail.com>
2021-07-13 11:15:09 +0200

    Adding 'addUser' endpoint
    Implementing handler accepting only POST method,
    which prints request body to the console

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -10,8 +11,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
+func addUserHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		reqBody, _ := ioutil.ReadAll(r.Body)
+		fmt.Println(string(reqBody))
+	default:
+		fmt.Fprintf(w, "Sorry, only POST method is supported.")
+	}
+}
+
+
 func runServer() {
 	log.Println("Starting server...")
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/addUser", addUserHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
